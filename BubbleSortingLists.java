76ba45d057908d36710f3b99bd85d571d3d46f65
Dansolo1235 <danielberihun92@gmail.com>
2024-03-05 22:34:18 -0600

    added the comments

--- BubbleSortingLists.java
+++ BubbleSortingLists.java
@@ -2,7 +2,10 @@ import java.util.ArrayList;
 
 public class BubbleSortingLists {
 
-
+//returns more than 1 if word1 comes after word2
+// returns 1 if word1 is similar to word2 but longer than it e.g., apple vs app
+// return -1 if word1 is similar to word2 but shorter than it e.g., app vs apple
+// return 0 if exactly identical
 public static int compareStrings(String word1, String word2) {
     int difference = 0;
     int asc1, asc2;
@@ -20,22 +23,28 @@ public static int compareStrings(String word1, String word2) {
             return difference;
     }
 
-  if(word1.length() > word2.length())    
-return 1;
-else if (word1.length() < word2.length())
-    return -1;
-else
-    return 0;
+    if(word1.length() > word2.length())    // word1 is longer
+    return 1;
+    else if (word1.length() < word2.length()) // word2 is longer
+        return -1;
+    else
+        return 0; // they are identical
 
 }
 
 public static void main(String[] args) {
         ArrayList<String> Values = new ArrayList<>();
         Values.add("Apple");
-        Values.add("PineApple");
+        Values.add("Fish");
+        Values.add("Carrot");
         Values.add("Banana");
-        Values.add("Sandia");
-        Values.add("Nada");
+        
+        Values.add("Honey");
+        Values.add("Icecream");
+        Values.add("Elephant");
+        Values.add("Honey");
+        Values.add("Grape");
+        Values.add("App");
         
      
          System.out.println("Before Sorting");
@@ -48,7 +57,7 @@ for(int i=0; i<Values.size()-1; i++){
 word1=Values.get(i);
 word2=Values.get(i+1);
 
-if(compareStrings(word1.toLowerCase(), word2.toLowerCase())>1){
+if(compareStrings(word1.toLowerCase(), word2.toLowerCase())>=1){
 continueSwapping=true;
 Values.set(i, word2);
 Values.set(i+1, word1);
@@ -59,6 +68,7 @@ System.out.println(Values);
 
 }
 }
+System.out.println("final version");
 System.out.println(Values); 
         
         }
